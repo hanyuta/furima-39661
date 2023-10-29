96e19cacbfa387f1e2009a1b66533420f9c4e193
hanyuta <hanyutatech24@gmail.com>
2023-10-29 12:13:55 +0900

    テストコード準備設定

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number:2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length:6)}
+    password_confirmation {password}
+    first_name            {"太郎"}
+    last_name             {"田中"}
+    first_name_kana       {"タロウ"}
+    last_name_kana        {"タナカ"}
+    birthday              {Faker::Date.birthday(min_age: 18,max_age: 60)}
+  end
+end
