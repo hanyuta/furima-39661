65d9b601595db23a0e0a377c68aa4343def49b31
hanyuta <hanyutatech24@gmail.com>
2023-10-29 23:25:25 +0900

    テストコードの修正と項目追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name            { '太郎' }
     last_name             { '田中' }
