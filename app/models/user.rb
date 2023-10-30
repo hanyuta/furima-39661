069091725d822ff10475e04660bcf29212028eb0
hanyuta <hanyutatech24@gmail.com>
2023-10-31 02:02:15 +0900

    正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,         presence: true
-  validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください' }
-  validates :last_name,        presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください' }
+  validates :first_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください' }
+  validates :last_name,        presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください' }
   validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
   validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
   validates :birthday,         presence: true
 
-  validates :password,         format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])/, message: 'は半角英数字混合を混同して設定してください' }
+  validates :password,         format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'は半角英数字混合を混同して設定してください' }
 end
