14cdb7fe5ef3c1ee81665db2278f63ad43e95d66
hanyuta <hanyutatech24@gmail.com>
2023-11-01 21:23:57 +0900

    itemsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_202845) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_01_121205) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "item_info", null: false
+    t.integer "item_category_id", null: false
+    t.integer "item_sales_status_id", null: false
+    t.integer "item_shipping_fee_staus_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "item_scheduled_delivery_id", null: false
+    t.integer "item_price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_202845) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
