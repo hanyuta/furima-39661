f590f88256d55d86740580c8fddaf51b67dd76fc
hanyuta <hanyutatech24@gmail.com>
2023-11-02 22:07:23 +0900

    createメソッド設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      render :new,status: :unprocessable_entitiy
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:item_name,:item_info,:item_category_id,:item_sales_status_id,:item_shipping_fee_staus_id,:prefecture_id,:item_scheduled_delivery_id,:item_price,:item_image),merge(user_id:current_user.id)
+    params.require(:item).permit(:item_name,:item_info,:item_category_id,:item_sales_status_id,:item_shipping_fee_staus_id,:prefecture_id,:item_scheduled_delivery_id,:item_price,:item_image).merge(user_id:current_user.id)
   end
+
 end
