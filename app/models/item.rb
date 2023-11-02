c3cf1e60b53e55037fc9d40d52ce5a7c800ae542
hanyuta <hanyutatech24@gmail.com>
2023-11-02 22:03:21 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,13 @@
 class Item < ApplicationRecord
   has_one_attached :item_image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :presence
+  belongs_to :user
 
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id,                       numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_category_id,                    numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_sales_status_id,                numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_shipping_fee_status_id,         numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_scheduled_delivery_id,          numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_name,:item_info,:item_price,    presence:true
+
 end
