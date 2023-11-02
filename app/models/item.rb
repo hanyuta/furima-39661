38034c35a3db9387300afad30c64838508b62b42
hanyuta <hanyutatech24@gmail.com>
2023-11-02 16:06:48 +0900

    発送元地域実装(ActiveHash)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :item_image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :presence
+
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 end
