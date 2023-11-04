8a99888e296716efff2a75838ce5e5cd584a40c7
hanyuta <hanyutatech24@gmail.com>
2023-11-05 03:02:44 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,11 @@ class Item < ApplicationRecord
 
   validates :item_image,                          presence:true
   validates :item_name,:item_info,                presence:true
-  validates :item_category_id,                    numericality: {other_than: 1, message: " can't be blank"}
-  validates :item_sales_status_id,                numericality: {other_than: 1, message: " can't be blank"}
-  validates :item_shipping_fee_staus_id,         numericality: {other_than: 1, message: " can't be blank"}
-  validates :prefecture_id,                       numericality: {other_than: 1, message: " can't be blank"}
-  validates :item_scheduled_delivery_id,          numericality: {other_than: 1, message: " can't be blank"}
-  validates :item_price,                          presence:true, format: {with: /\A[1-9]\d{2,6}\z/, message: "is only numbers" }  
+  validates :item_category_id,                    numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_sales_status_id,                numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_shipping_fee_staus_id,          numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id,                       numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_scheduled_delivery_id,          numericality: {other_than: 1, message: "can't be blank"}
+  validates :item_price,                          presence:true, format: {with: /\A[3-9][0-9]{2,6}\z/, message: "is only numbers" }  
 
 end
