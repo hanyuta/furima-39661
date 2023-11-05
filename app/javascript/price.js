77069a5ef66f5375e475bc2120fa9d5ee2385adb
hanyuta <hanyutatech24@gmail.com>
2023-11-05 16:58:27 +0900

    小数点計算をMath.floor()メソッドに修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -10,8 +10,8 @@ function SalesProfit(){
     const tax= 0.1*price;
     const profit= price -tax;
 
-    document.getElementById('add-tax-price').textContent=tax.toFixed(0);
-    document.getElementById('profit').textContent=profit.toFixed(0);
+    document.getElementById('add-tax-price').textContent=Math.floor(tax);
+    document.getElementById('profit').textContent=Math.floor(profit);
   }else{
     document.getElementById('add-tax-price').textContent='NaN';
     document.getElementById('profit').textContent='NaN';
