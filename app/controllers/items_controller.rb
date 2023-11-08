9810d5ecb6c9549bf0a2a5101ee8fb2cfd406edf
hanyuta <hanyutatech24@gmail.com>
2023-11-09 01:24:03 +0900

    beforeアクションの記述位置修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
-class ItemsController < ApplicationController   before_action :authenticate_user!, only: [:new ,:edit]
+class ItemsController < ApplicationController   
+  before_action :authenticate_user!, only: [:new ,:edit]
 
   def index
     @items = Item.order(created_at: :desc)
