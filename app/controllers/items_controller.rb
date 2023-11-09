de493ac5a676c1c26749fa178a22ed10ac9cf293
hanyuta <hanyutatech24@gmail.com>
2023-11-10 03:39:52 +0900

    findメソッド統合

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController   
   before_action :authenticate_user!, only: [:new ,:edit]
+  before_action :set_item, only: [:edit , :show]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -10,7 +11,7 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def create
@@ -24,15 +25,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user == User.find(@item.user_id)
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
-
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -45,5 +43,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :item_info, :item_category_id, :item_sales_status_id, :item_shipping_fee_staus_id,
                                  :prefecture_id, :item_scheduled_delivery_id, :item_price, :item_image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
