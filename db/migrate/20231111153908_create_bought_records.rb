27bcac85cebd11bb8306ec76f2fbb0ca7c64beec
hanyuta <hanyutatech24@gmail.com>
2023-11-12 00:50:11 +0900

    bought_recordモデルのマイグレーション設定

--- db/migrate/20231111153908_create_bought_records.rb
+++ db/migrate/20231111153908_create_bought_records.rb
@@ -1,7 +1,8 @@
 class CreateBoughtRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :bought_records do |t|
-
+      t.references :user , null:false ,foreign_key:true
+      t.references :item , null:false ,foreign_key:true
       t.timestamps
     end
   end
