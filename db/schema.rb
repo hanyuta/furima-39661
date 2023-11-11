13d5b327fa723bba4da2a65948f87b161b9b33c1
hanyuta <hanyutatech24@gmail.com>
2023-11-12 01:31:07 +0900

    itemマイグレーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_01_205116) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_11_153908) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_01_205116) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "bought_records", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_bought_records_on_item_id"
+    t.index ["user_id"], name: "index_bought_records_on_user_id"
   end
 
   create_table "items", charset: "utf8", force: :cascade do |t|
@@ -74,5 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_01_205116) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bought_records", "items"
+  add_foreign_key "bought_records", "users"
   add_foreign_key "items", "users"
 end
