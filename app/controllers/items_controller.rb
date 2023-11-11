7a96368e984773c30a10afe9098c53d6c2510506
hanyuta <hanyutatech24@gmail.com>
2023-11-11 17:25:36 +0900

    updateメソッド修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,6 +31,7 @@ class ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
