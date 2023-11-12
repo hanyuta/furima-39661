5a4d9af52fd55707c03430f961627012951c218f
hanyuta <hanyutatech24@gmail.com>
2023-11-13 07:34:32 +0900

    テストコード"入力内容に問題がない場合"を記述

--- spec/models/shipping_address_spec.rb
+++ spec/models/shipping_address_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe ShippingAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @shipping_address = FactoryBot.build(:shipping_address)
+  end
+
+  describe "入力確認" do
+    context "内容に問題がない場合" do
+      it "postal_code,city,address,phone_numberが空でない、且つprefecture_idが1以外が選択されている" do
+        expect(@shipping_address).to be_valid
+      end
+    end
+
+    context "内容に問題がある場合" do
+      it "postal_codeが空では登録できない" do
+      end
+      it "cityが空では登録できない" do
+      end
+      it "addressが空では登録できない" do
+      end
+      it "phone_numberが空では登録できない" do
+      end
+      it "prefecture_idが1(---)では登録できない" do
+      end
+    end
+
+  end
 end
