b1e3834d8f873e6fe8f280bae9ef09926b97831e
hanyuta <hanyutatech24@gmail.com>
2023-11-13 07:33:47 +0900

    factorybot生成コード修正

--- spec/factories/shipping_addresses.rb
+++ spec/factories/shipping_addresses.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :shipping_address do
-    postal_code    {Faker::Address.postcode}
+    postal_code    {"111-1111"}
     prefecture_id  {2}
     city           {Faker::Address.city}
-    address        {Faker::Address.address}
-    building       {Faker::Address.building}
-    phone_number   {Faker::PhoneNumber.phone_number}
+    address        {Faker::Address.street_address}
+    phone_number   {11111111111}
     association :bought_record
   end
 end
