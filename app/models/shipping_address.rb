388bf781dfa607849591e8ede1fc5b4418d4f931
hanyuta <hanyutatech24@gmail.com>
2023-11-12 19:07:01 +0900

    バリデーション設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,8 @@
 class ShippingAddress < ApplicationRecord
-end
+
+
+  validates :postal_code,    presence: true ,format:{ with: /\A[0-9]{3}-[0-9]{4}\Z/, message: "is invalid.Enter it as follows (e.g. 123-4567)"}
+  validates :prefecture_id,  presence: true
+  validates :city,           presence: true
+  validates :address,        presence: true
+  validates :phone_number,   presence: true ,numericality: {only_integer: true, greater_than_or_equal_to:9_999_999_999}
