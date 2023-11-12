6e8d8f379af6440bd7c4237cf298d0cf8f767d41
hanyuta <hanyutatech24@gmail.com>
2023-11-12 23:38:24 +0900

    テストコード記述ファイル移行

--- spec/factories/shipping_addresses.rb
+++ spec/factories/shipping_addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :shipping_address do
-    
+    postal_code    {Faker::Address.postcode}
+    prefecture_id  {2}
+    city           {Faker::Address.city}
+    address        {Faker::Address.address}
+    building       {Faker::Address.building}
+    phone_number   {Faker::PhoneNumber.phone_number}
+    association :bought_record
   end
 end
