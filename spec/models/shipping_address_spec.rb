0f1770cd0ace8970f7a483c81b8178bde2f470a5
hanyuta <hanyutatech24@gmail.com>
2023-11-13 21:20:57 +0900

    単体テスト、内容に問題がある場合の記述

--- spec/models/shipping_address_spec.rb
+++ spec/models/shipping_address_spec.rb
@@ -14,14 +14,39 @@ RSpec.describe ShippingAddress, type: :model do
 
     context "内容に問題がある場合" do
       it "postal_codeが空では登録できない" do
+        @shipping_address.postal_code = ''
+        @shipping_address.valid?
+        expect(@shipping_address.errors.full_messages).to include "Postal code can't be blank"
       end
       it "cityが空では登録できない" do
+        @shipping_address.city = ''
+        @shipping_address.valid?
+        expect(@shipping_address.errors.full_messages).to include "City can't be blank"
       end
       it "addressが空では登録できない" do
+        @shipping_address.address = ""
+        @shipping_address.valid?
+        expect(@shipping_address.errors.full_messages).to include "Address can't be blank"
       end
       it "phone_numberが空では登録できない" do
+        @shipping_address.phone_number = ""
+        @shipping_address.valid?
+        expect(@shipping_address.errors.full_messages).to include "Phone number can't be blank"
       end
       it "prefecture_idが1(---)では登録できない" do
+        @shipping_address.prefecture_id = 1
+        @shipping_address.valid?
+        expect(@shipping_address.errors.full_messages).to include "Prefecture can't be blank"
+      end
+      it "postal_codeの入力が正しくなければ登録できない" do
+        @shipping_address.postal_code = 1111111
+        @shipping_address.valid?
+        expect(@shipping_address.errors.full_messages).to include "Postal code is invalid.Enter it as follows (e.g. 123-4567)"
+      end
+      it "phone_numberが数字以外では登録できない" do
+        @shipping_address.phone_number = "abc"
+        @shipping_address.valid?
+        expect(@shipping_address.errors.full_messages).to include "Phone number is not a number"
       end
     end
 
