67f9334b02160f462de576668dc98f28d1998e7c
hanyuta <hanyutatech24@gmail.com>
2023-11-14 22:48:18 +0900

    tokenをshipping_addressモデルで取り扱えるようにする

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,5 +1,6 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :bought_record
+  attr_accessor :token
 
   validates :postal_code,    presence: true ,format:{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid.Enter it as follows (e.g. 123-4567)"}
   validates :prefecture_id,  presence: true ,numericality: { other_than: 1,message: "can't be blank" }
