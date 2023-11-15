769fb8df17378d457254a35ad0cfae69da0d4195
hanyuta <hanyutatech24@gmail.com>
2023-11-15 22:16:22 +0900

    ファクトリーボットの記述移動

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :order do
-    
+    postal_code    {"111-1111"}
+    prefecture_id  {2}
+    city           {Faker::Address.city}
+    address        {Faker::Address.street_address}
+    phone_number   {11111111111}
+    association :bought_record
   end
 end
--- spec/factories/shipping_addresses.rb
+++ spec/factories/shipping_addresses.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :shipping_address do
-    postal_code    {"111-1111"}
-    prefecture_id  {2}
-    city           {Faker::Address.city}
-    address        {Faker::Address.street_address}
-    phone_number   {11111111111}
-    association :bought_record
+
   end
 end
