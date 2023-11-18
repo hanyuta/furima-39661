9be5c3290b085043a71d79628837c11476deee2d
hanyuta <hanyutatech24@gmail.com>
2023-11-18 19:41:42 +0900

    tokenの条件追加

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     city           {Faker::Address.city}
     address        {Faker::Address.street_address}
     phone_number   {11111111111}
+    token          {"tok_abcdefghijk00000000000000000"}
     association :bought_record
   end
 end
