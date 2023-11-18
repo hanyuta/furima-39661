b69b302dac77737d5b2bcded4cb003f1e66f8330
hanyuta <hanyutatech24@gmail.com>
2023-11-18 11:27:32 +0900

    バリデーションtoken追加、他バリデーション修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,8 @@ class Order
   attr_accessor :postal_code,:prefecture_id,:city,:address,:building,:phone_number,:bought_record_id,:user_id,:item_id,:token
 
   with_options presence: true do
+    validates :token
     validates :postal_code,    format:{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid.Enter it as follows (e.g. 123-4567)"}
-    validates :prefecture_id,  numericality: { other_than: 1,message: "can't be blank" }
     validates :city
     validates :address
     validates :phone_number,   numericality: {only_integer: true, greater_than_or_equal_to:9_999_999_999}
@@ -12,6 +12,8 @@ class Order
     validates :item_id
     validates :bought_record_id
   end
+
+  validates :prefecture_id,  numericality: { other_than: 1,message: "can't be blank" }
 
   def save
     bought_record = BoughtRecord.create(user_id: user_id,item_id: item_id)
